0413ebb621526416d6cb97a1b60dadee104e99a3
Rakshith-Ramachandra <rakshithr028.o@gmail.com>
2026-06-03 22:41:43 +0530

    REACT-QUIZ fetched questions,practise useReduceer

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,7 +1,37 @@
-import DateCounter from "./DateCounter"
+import { useEffect, useReducer } from "react"
+import Header from "./Header"
+import Main from "./main"
+
+// const initialState = {
+//   questions: [],
+//   status: "",
+// }
+
+// function reducer(state, action) {
+//   switch(action.type) {
+//     case "dataFetched":
+//       return {...state, payLoad: }
+//   }
+// }
 
 function App() {
-  return <DateCounter />
+  // const [state, dispatch] = useReducer(reducer, initialState)
+
+  // useEffect(function () {
+  //   fetch("http://localhost:8000/questions")
+  //     .then((res) => res.json())
+  //     .then((data) => dispatch({type}))
+  // }, [])
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
+    </div>
+  )
 }
 
 export default App
