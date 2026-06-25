711efe3bfeaf479f6a4d9d28e8a23cd0cb1d3e39
Rakshith-Ramachandra <rakshithr028.o@gmail.com>
2026-06-25 22:11:02 +0530

    WORLDWISE - Completed React Router / Section 17

--- worldWise/src/App.jsx
+++ worldWise/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Product from "./pages/Product"
 import Homepage from "./pages/Homepage"
 import Pricing from "./pages/Pricing"
@@ -6,7 +7,9 @@ import PageNotFound from "./pages/PageNotFound"
 import Login from "./pages/Login"
 import AppLayout from "./pages/AppLayout"
 import CityList from "./components/CityList"
-import { useEffect, useState } from "react"
+import CountryList from "./components/CountryList"
+import City from "./components/City"
+import Form from "./components/Form"
 
 const BASE_URL = "http://localhost:9000"
 
@@ -38,21 +41,17 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} loading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} loading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
-            element={<p>Countries</p>}
-            cities={cities}
-            loading={isLoading}
+            element={<CountryList cities={cities} loading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- worldWise/src/components/Map.jsx
+++ worldWise/src/components/Map.jsx
@@ -1,6 +1,32 @@
 import React from "react"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import styles from "./Map.module.css"
 
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>
+  const navigate = useNavigate()
+
+  const [searchParams, setSearchParams] = useSearchParams()
+  const lat = searchParams.get("lat")
+  const lng = searchParams.get("lng")
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form")
+      }}
+    >
+      <h1>Map</h1>
+      <p>
+        Position: {lat},{lng}
+      </p>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 25, lng: 62 })
+        }}
+      >
+        Change pos
+      </button>
+    </div>
+  )
 }
